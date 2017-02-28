1819bde69ba8a0f45af66b4b164f7048f7675df4
Shaobin Tao <shaobin_tao@hotmail.com>
2017-02-27 21:11:56 -0800

    public Android Studio + Native OpenGL test

---
+++ NativeTest/app/src/main/java/com/example/s00407713/nativetest/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.s00407713.nativetest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements SurfaceHolder.Callback
+{
+    private static String TAG = "EglSample";
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate()");
+
+        setContentView(R.layout.activity_main);
+
+        SurfaceView surfaceView = (SurfaceView)findViewById(R.id.surfaceview);
+        surfaceView.getHolder().addCallback(this);
+        surfaceView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(MainActivity.this,
+                        "This demo combines Java UI and native EGL + OpenGL renderer",
+                        Toast.LENGTH_LONG);
+                toast.show();
+            }});
+
+        // Example of a call to a native method
+//        TextView tv = (TextView) findViewById(R.id.sample_text);
+//        tv.setText(stringFromJNI());
+    }
+
+
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart()");
+        nativeOnStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume()");
+        nativeOnResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause()");
+        nativeOnPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop()");
+        nativeOnStop();
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        nativeSetSurface(holder.getSurface());
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        nativeSetSurface(null);
+    }
+
+
+    public static native void nativeOnStart();
+    public static native void nativeOnResume();
+    public static native void nativeOnPause();
+    public static native void nativeOnStop();
+    public static native void nativeSetSurface(Surface surface);
+
+}
+
+
