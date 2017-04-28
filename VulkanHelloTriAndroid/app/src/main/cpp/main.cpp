17ba4b69d7be5d9ada81fb1cc5aa96f4544a0f98
Shaobin Tao <shaobin_tao@hotmail.com>
2017-04-28 15:57:59 -0700

    VulkanHelloTriAndroid: stub changes for using NativeActivity

---
+++ VulkanHelloTriAndroid/app/src/main/cpp/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by s00407713 on 4/28/2017.
+//
+#include <cstdlib>
+#include <android_native_app_glue.h>
+
+
+void ANativeActivity_onCreate(ANativeActivity *activity, void *savedState, size_t savedStateSize)
+{
+
+}
