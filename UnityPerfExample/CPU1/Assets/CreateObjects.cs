147b11da6bbcc61def8e5b934c6e69c458f9730c
Shaobin Tao <shaobin_tao@hotmail.com>
2017-06-06 11:46:02 -0700

    CPU1

---
+++ UnityPerfExample/CPU1/Assets/CreateObjects.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObjects : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		for (int i = 0; i < 100; i++) {
+			GameObject	obj = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+
+			float x = Random.Range (0.0f, 50.0f);
+			float y = Random.Range (0.0f, 50.0f);
+			float z = Random.Range (0.0f, 50.0f);
+			obj.transform.localPosition = new Vector3 (x, y, z);
+
+			obj.SetActive(false);
+
+			Destroy (obj, 1);
+		}
+
+
+
+	}
+}
