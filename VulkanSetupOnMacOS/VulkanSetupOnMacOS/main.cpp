5dca5dfabada88eaddf3a1780ce591f7c4d69f48
Shaobin Tao <shaobin_tao@apple.com>
2024-10-16 09:58:21 -0700

    Framebuffers

--- VulkanSetupOnMacOS/VulkanSetupOnMacOS/main.cpp
+++ VulkanSetupOnMacOS/VulkanSetupOnMacOS/main.cpp
@@ -99,6 +99,7 @@ private:
     VkRenderPass renderPass;
     VkPipelineLayout pipelineLayout;
     VkPipeline graphicsPipeline;
+    std::vector<VkFramebuffer> swapChainFramebuffers;
 
     
     void setupDebugMessenger() {
@@ -679,6 +680,27 @@ VkRenderPassCreateInfo renderPassInfo{};
         
         
     }
+    void createFramebuffers() {
+        for (size_t i = 0; i < swapChainImageViews.size(); i++) {
+            VkImageView attachments[] = {
+                swapChainImageViews[i]
+            };
+
+            VkFramebufferCreateInfo framebufferInfo{};
+            framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+            framebufferInfo.renderPass = renderPass;
+            framebufferInfo.attachmentCount = 1;
+            framebufferInfo.pAttachments = attachments;
+            framebufferInfo.width = swapChainExtent.width;
+            framebufferInfo.height = swapChainExtent.height;
+            framebufferInfo.layers = 1;
+
+            if (vkCreateFramebuffer(device, &framebufferInfo, nullptr, &swapChainFramebuffers[i]) != VK_SUCCESS) {
+                throw std::runtime_error("failed to create framebuffer!");
+            }
+        }
+    }
+    
     void initVulkan() {
         createInstance();
         setupDebugMessenger();
@@ -689,8 +711,13 @@ VkRenderPassCreateInfo renderPassInfo{};
         createImageViews();
         createRenderPass();
         createGraphicsPipeline();
+        createFramebuffers();
     }
     void cleanup() {
+        for (auto framebuffer : swapChainFramebuffers) {
+            vkDestroyFramebuffer(device, framebuffer, nullptr);
+        }
+        
         vkDestroyPipeline(device, graphicsPipeline, nullptr);
 
         vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
